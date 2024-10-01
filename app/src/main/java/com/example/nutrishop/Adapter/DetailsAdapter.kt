aaa566a776dbf39c130a8d6395a914fca44456d9
Charindu Ranasingh <charindu2001@gmail.com>
2024-10-01 22:42:12 +0530

    Details adapter update

--- app/src/main/java/com/example/nutrishop/Adapter/DetailsAdapter.kt
+++ app/src/main/java/com/example/nutrishop/Adapter/DetailsAdapter.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.request.RequestOptions
 import com.example.nutrishop.Model.DetailsModel
 import com.example.nutrishop.databinding.ViewholderDetailsBinding
 
@@ -19,12 +22,15 @@ class DetailsAdapter(val items: MutableList<DetailsModel>) :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailsAdapter.ViewHolder {
         context = parent.context
         val binding = ViewholderDetailsBinding.inflate(LayoutInflater.from(context), parent, false)
-        return Viewholder(binding)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: DetailsAdapter.ViewHolder, position: Int) {
         holder.binding.nutrientTxt.text = items[position].nutrient
         holder.binding.amountTxt.text = items[position].amount
+
+        val requestOptions = RequestOptions().transform(CenterCrop())
+
 
 
     }
